8d1d7ea068c89e0dd2fba7cc13e65d852c4a26e3
Ser-en-dipity <610511058@qq.com>
2024-05-26 19:43:12 +0800

    select point

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,7 +9,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using SciChart.Charting.Model.DataSeries;
-using System;
+using System.ComponentModel;
+using SciChart.Charting;
 
 namespace Sci_Chart;
 
@@ -21,9 +22,41 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        // var dataSeries = new XyDataSeries<double, double>();
+        // for(int i = 0; i < 4096; i++)
+        //     dataSeries.Append(i, Math.Sin(i*0.2));
+        // lineSeries.DataSeries = dataSeries;
+    }
+
+    private void PointMarkersSelectionExampleView_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        
         var dataSeries = new XyDataSeries<double, double>();
-        for(int i = 0; i < 100; i++)
-            dataSeries.Append(i, Math.Sin(i*0.2));
+        for(int i = 0; i < 4096; i++)
+        {
+            dataSeries.Append(i, Math.Sin(i*0.2), new SelectedPointMetadata() { IsSelected = false });
+            
+        }
+        
+        // var data = DataManager.Instance.GetSinewave(100,55,150);
+
+       // Append data to series. SciChart automatically redraws
+        // dataSeries.Append(new double[] { 0,1,2,3 }, new double[] { 10,20,30,40 },
+        //     new [] { 
+        //     new SelectedPointMetadata() { IsSelected = true },
+        //         new SelectedPointMetadata() { IsSelected = false },
+        //         new SelectedPointMetadata() { IsSelected = false },
+        //         new SelectedPointMetadata() { IsSelected = false },
+        // } );
         lineSeries.DataSeries = dataSeries;
+
+        sciChart.ZoomExtents();
+        
+    }
+
+    public class SelectedPointMetadata : IPointMetadata
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public bool IsSelected { get; set; }
     }
 }
